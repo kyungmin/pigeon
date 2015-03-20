f0a49f12292e3917787f14d7834f3134626615a0
Kyungmin Kim <kmin905@gmail.com>
2015-03-20 07:43:21 -1000

    More font interactions

--- Pigeon/Pigeon/FontViewController.swift
+++ Pigeon/Pigeon/FontViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class FontViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-    var image: UIImage!
     var selectedFont: String!
     var fonts: [String]! = []
     var onDataAvailable : ((data: String) -> ())?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        imageView.image = image
     }
 
     @IBAction func didSelectFont(sender: AnyObject) {
@@ -30,12 +26,6 @@ class FontViewController: UIViewController {
     
     @IBAction func didTapBackground(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func sendData(data: String) {
-        // Whenever you want to send data back to viewController1, check
-        // if the closure is implemented and then call it if it is
-        self.onDataAvailable?(data: data)
     }
     
     override func didReceiveMemoryWarning() {
--- Pigeon/Pigeon/FrontEditViewController.swift
+++ Pigeon/Pigeon/FrontEditViewController.swift
@@ -19,6 +19,10 @@ class FrontEditViewController: UIViewController, UIViewControllerTransitioningDe
     var originalImageCenter: CGPoint!
     var currentSelection: AnyObject!
     var imageTransition: ImageTransition!
+    var textField: UITextField!
+    
+    var tabs: [UIViewController]!
+    var segues: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,19 +30,40 @@ class FrontEditViewController: UIViewController, UIViewControllerTransitioningDe
         originalImageFrame = imageView.frame
         originalImageCenter = imageView.center
         scrollView.contentSize = imageView.frame.size
+        
+        tabs = [FontViewController, StickerViewController, TemplateViewController]
+        segues = ["fontSegue", "stickerSegue", "templateSegue"]
     }
 
     @IBAction func didPinchImage(sender: UIPinchGestureRecognizer) {
         scale = sender.scale
+        var target = sender.view!
         
         if (sender.state == UIGestureRecognizerState.Changed) {
-            imageView.transform = CGAffineTransformScale(sender.view!.transform, scale, scale)
+            target.transform = CGAffineTransformScale(target.transform, scale, scale)
         } else if (sender.state == UIGestureRecognizerState.Ended) {
-            if (sender.view!.frame.size.width < originalImageFrame.size.width) {
-                imageView.transform = CGAffineTransformMakeScale(1, 1)
+            if (target.frame.size.width < originalImageFrame.size.width) {
+                target.transform = CGAffineTransformMakeScale(1, 1)
             }
         }
         sender.scale = 1
+    }
+
+    
+    @IBAction func didPinchLabel(sender: UIPinchGestureRecognizer) {
+        imageView.userInteractionEnabled = false
+        
+        scale = sender.scale
+        //TODO: better handle scaling back
+        
+        var label = sender.view as UILabel
+        
+        if (sender.state == UIGestureRecognizerState.Changed) {
+            label.font = label.font.fontWithSize(label.font.pointSize + scale)
+        } else if (sender.state == UIGestureRecognizerState.Ended) {
+            imageView.userInteractionEnabled = true
+        }
+        
     }
     
     @IBAction func didPanImage(sender: UIPanGestureRecognizer) {
@@ -71,36 +96,60 @@ class FrontEditViewController: UIViewController, UIViewControllerTransitioningDe
 
     @IBAction func didTapLabel(sender: UIPanGestureRecognizer) {
         var label = sender.view as UILabel
-        label.textColor = UIColor.blueColor()
         currentSelection = label
-        addPanGestureRecognizer(label)
-        label.userInteractionEnabled = true
-        imageView.userInteractionEnabled = false
+        
+        textField = UITextField(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        label.alpha = 0
+        textField.center = label.center
+        textField.textAlignment = .Center
+        textField.text = label.text
+        textField.textColor = UIColor.whiteColor()
+        scrollView.addSubview(textField)
+        // TODO: enable editing
+        // TODO: select all text
     }
     
-    @IBAction func didPressFontButton(sender: AnyObject) {
-        performSegueWithIdentifier("fontSegue", sender: self)
+    @IBAction func didTapBackground(sender: AnyObject) {
+        view.endEditing(true)
+        var label = currentSelection as UILabel
+        label.text = textField.text
+        label.alpha = 1
+        textField.removeFromSuperview()
     }
     
     func addFont(selectedFont: String) {
-        var newLabel = UILabel(frame: CGRectMake(0, 0, 200, 50))
+        var newLabel = UILabel(frame: CGRectMake(0, 0, 100, 50))
         newLabel.font = UIFont(name: selectedFont, size: CGFloat(50))
-        newLabel.text = "Font 1"
+        newLabel.text = "Your text"
         newLabel.textColor = UIColor.whiteColor()
         newLabel.textAlignment = .Center
-        newLabel.userInteractionEnabled = true
         newLabel.center = imageView.center
-        addTapGestureRecognizer(newLabel)
+        newLabel.userInteractionEnabled = true
+        imageView.userInteractionEnabled = false
+        currentSelection = newLabel
 
+        addTapGestureRecognizer(newLabel)
+        addPanGestureRecognizer(newLabel)
+        addPinchGestureRecognizer(newLabel)
+        
         scrollView.addSubview(newLabel)
     }
-    
+
+    // scaling text
+    func addPinchGestureRecognizer(target :AnyObject) {
+        var pinchGesture = UIPinchGestureRecognizer(target: self, action: "didPinchLabel:")
+        pinchGesture.delegate = self
+        target.addGestureRecognizer(pinchGesture)
+    }
+
+    // editing text
     func addTapGestureRecognizer(target :AnyObject) {
         var tapGesture = UITapGestureRecognizer(target: self, action: "didTapLabel:")
         tapGesture.delegate = self
         target.addGestureRecognizer(tapGesture)
     }
 
+    // moving text
     func addPanGestureRecognizer(target :AnyObject) {
         var panGesture = UIPanGestureRecognizer(target: self, action: "didPanLabel:")
         panGesture.delegate = self
@@ -112,15 +161,20 @@ class FrontEditViewController: UIViewController, UIViewControllerTransitioningDe
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func didPressFontButton(sender: AnyObject) {
+        performSegueWithIdentifier("fontSegue", sender: self)
+    }
     
-
-
+    @IBAction func didPressStickerButton(sender: AnyObject) {
+        performSegueWithIdentifier("stickerSegue", sender: self)
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         var toViewController = segue.destinationViewController as FontViewController
-        toViewController.image = imageView.image
         toViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
 
         imageTransition = ImageTransition()
